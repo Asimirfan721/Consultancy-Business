52d47d5187a132f68683060f7930c6a7b1d425a3
Asim Irfan <rajputasim721@gmail.com>
2025-07-14 23:24:00 +0500

    here it's the really issue

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -18,7 +18,7 @@ class BlogController extends Controller
 
         abort(404); // page not found for unknown slug
     }
-    public function studyInItaly()
+    public function studyInItaly() // this method handles the route for Italy blog and showing an error related to page 404 not found
 {
     return view('blogs.italy');
 }
