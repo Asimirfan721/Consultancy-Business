d0f3bc917896940f54d00e879e6aa8136972f7d9
Asim Irfan <rajputasim721@gmail.com>
2025-07-16 15:59:19 +0500

    view not found issue resolved

--- app/Http/Controllers/ScholarshipController.php
+++ app/Http/Controllers/ScholarshipController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class ScholarshipController extends Controller
 {
    public function index(){
-    return view('Schoalrship.index');
+    return view('Scholarship.index');
    }
    public function anso()
 {
